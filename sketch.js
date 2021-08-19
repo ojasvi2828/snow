ce65fc7ab76440262aa386e3e5b0bf5426c78cee
ojasvi2828 <77545304+ojasvi2828@users.noreply.github.com>
2021-08-19 03:08:16 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,22 +1,27 @@
-
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine, world;
 var particles = [];
 var snow;
 function setup() {
   createCanvas(800,400);
     //paricles.createSprite();
   bg =loadImage("snow2.jpg");
-
+  engine = Engine.create();
+  world = engine.world;
 
 }
 
 function draw() {
   background(bg);  
   
-
+  Engine.update(engine);
 
 
   if(frameCount%60 == 0){
-    snow.push(new Snow(random(300,700),5));
+    particles.push(new Snow(random(50,750),random(0,350)));
   
   }
   for (var i = 0; i < particles.length; i++) {
--- snow.js
+++ snow.js
@@ -2,13 +2,12 @@ class Snow{
     constructor(x, y) {
         var options = {
             restitution: 1,
-            friction: 0,
+            friction: 2 ,
             isStatic:false
         }
-        this.r = 10;
+        this.r = 25;
         this.image=loadImage("snow5.webp")
-        this.body = Bodies.circle(x, y, this.r, options);
-        this.color =  color(random(0,255),random(0,255),random(0,255));
+        this.body = Bodies.circle(x, y, this.r/2, options);
         World.add(world, this.body);
     }
     display() {
@@ -20,10 +19,7 @@ class Snow{
         translate(pos.x, pos.y);
         rotate(angle);
         imageMode(CENTER);
-        noStroke();
-        fill(this.color);
-        ellipseMode(RADIUS);
-		image(this.image, 0,0,this.r)
+		image(this.image, 0,0,this.r,this.r);
         pop();
     }
 
